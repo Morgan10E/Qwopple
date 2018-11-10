6bdd2efaebb66b88b82437149dfa9aea532bc357
Morgan Tenney <morgan.tenney@gmail.com>
2018-11-09 23:00:50 -0800

    updated joystick input

--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -11,8 +11,8 @@ public class InputTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log(Input.GetAxis("LeftHorizontal") +", " + Input.GetAxis("LeftVertical") + "    " + Input.GetAxis("RightHorizontal") +", " + Input.GetAxis("RightVertical"));
+		Debug.Log(Input.GetAxis("LeftHorizontal") +", " + Input.GetAxis("LeftVertical") + "    " + Input.GetAxis("RightHorizontal") +", " + Input.GetAxis("RightVertical"));
 		// Debug.Log(Input.GetAxis("LeftTrigger") +", " + Input.GetAxis("RightTrigger")); // note from Morgan - looks like these chill at 0 until the triggers are pressed, and then their baseline is -1. Weird.
-		Debug.Log(Input.GetButton("LeftBumper") + ", " + Input.GetButton("RightBumper"));
+		// Debug.Log(Input.GetButton("LeftBumper") + ", " + Input.GetButton("RightBumper"));
 	}
 }
