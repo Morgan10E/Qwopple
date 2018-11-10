da19698f43aaa93462c01df013eba2a52c40d5f7
benhannel <ben@bensprojects.com>
2018-11-10 13:09:32 -0800

    RelativeJoint2D grapple control, top half of ragdoll

--- Assets/Scripts/HandControl.cs
+++ Assets/Scripts/HandControl.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,23 +7,23 @@ public class HandControl : MonoBehaviour {
 	public string whichHand = "Left";
 	public float radius = 1.0f;
 
-	public Rigidbody2D rigidBody;
+	public RelativeJoint2D arm;
+	public Rigidbody2D shoulder;
 
-	Vector3 origin;
+    Camera cam;
 
-	// Use this for initialization
-	void Start () {
-		// use our starting point as the center point for our range of motion
-		Debug.Log(origin);
-	}
+    void Start()
+    {
+    	cam = Camera.main;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		float horizontal = Input.GetAxis(whichHand + "Horizontal");
-		float vertical = Input.GetAxis(whichHand + "Vertical");
-
-		Debug.Log(origin + new Vector3(horizontal * radius, vertical * radius, 0));
-		rigidBody.MovePosition(origin + new Vector3(horizontal * radius, vertical * radius, 0));
+		Vector3 shoulderPos = cam.WorldToScreenPoint(shoulder.position);
+		Vector2 mouse = new Vector2(Input.mousePosition.x - shoulderPos.x, Input.mousePosition.y - shoulderPos.y);
+		arm.linearOffset = -3 * mouse / mouse.magnitude;
+		// arm.angularOffset = 
+		// rigidBody.MovePosition(origin + new Vector3(horizontal * radius, vertical * radius, 0));
 		// rigidBody.position = origin + new Vector3(horizontal * radius, vertical * radius, 0);
 	}
 
