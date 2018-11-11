6cffba3864835fdc5c30eab9f6d7bfc05b073c19
Morgan Tenney <morgan.tenney@gmail.com>
2018-11-10 22:40:21 -0800

    Added a distance counter - potential objective is qwop-like get as far as you can to the right?

---
+++ Assets/Scripts/DistanceCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceCounter : MonoBehaviour {
+
+	public GameObject player;
+	private float playerStartingPosition;
+	private Text distanceCounter;
+
+	// Use this for initialization
+	void Start () {
+		playerStartingPosition = player.transform.position.x;
+		distanceCounter = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distanceCounter.text = (player.transform.position.x - playerStartingPosition).ToString("0.##");
+	}
+}
